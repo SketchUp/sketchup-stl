0d33a49e97ad368cf839a9a1040491155f41a64d
Thomas Thomassen <thomas@thomthom.net>
2013-05-29 21:47:46 +0200

    Added  Dutch translation. Thanks to Jan "brewsky"

---
+++ src/sketchup-stl/strings/nl/STL.rb
@@ -0,0 +1,94 @@
+// path = CommunityExtensions::STL::PLUGIN_STRINGS_PATH
+// options = { :custom_path => path }
+// tr = CommunityExtensions::STL::Translator.new("STL.strings", options)
+
+
+// Extension Description ///////////////////////////////////////////////////////
+"STL Import & Export" = "STL Import & Export";
+
+"Adds STL file format import and export. " +
+"This is an open source project sponsored by the SketchUp team. More " +
+"info and updates at https://github.com/SketchUp/sketchup-stl"
+=
+"Voegt import en export van STL-bestanden toe. " +
+"Dit is een open source project dat wordt gesponsord door het SketchUp team. " +
+"Meer informatie en updates op https://github.com/SketchUp/sketchup-stl";
+
+
+// Common //////////////////////////////////////////////////////////////////////
+"Meters" = "Meter";
+"Centimeters" = "Centimeter";
+"Millimeters" = "Millimeter";
+"Feet" = "Voet";
+"Inches" = "Inch";
+
+
+// Importer ////////////////////////////////////////////////////////////////////
+"STL Import" = "STL Import";
+"Importing geometry..." = "Importeren van geometrie...";
+"No geometry was imported." = "Er is geen geometrie geïmporteerd.";
+"Repairing geometry..." = "Repareren van geometrie...";
+"Cleaning up geometry..." = "Opschonen van geometrie...";
+"Importing STL done!" = "Importeren van STL is gereed!";
+
+// Import Options
+"Import STL Options" = "Import opties voor STL";
+"Geometry" = "Geometrie";
+"Merge coplanar faces" = "Voeg coplanaire vlakken samen";
+"Scale" = "Schaal";
+"Units:" = "Eenheid:";
+"Preserve drawing origin" = "Behoud de oorsprong";
+"Accept" = "OK";
+"Cancel" = "Annuleren";
+
+// Binary Import
+"STL Importer (c) Jim Foltz
+
+STL Binary Header:
+%s
+
+Found %i triangles. Continue?"
+=
+"STL Importer (c) Jim Foltz
+
+STL Binaire Koptekst:
+%s
+
+%i driehoeken gevonden. Doorgaan?";
+
+// ASCII Import
+"STL Importer (c) Jim Foltz
+
+STL ASCII File
+Found %i polygons.
+
+Continue?"
+=
+"STL Importer (c) Jim Foltz
+
+STL ASCII File
+%i veelhoeken gevonden.
+
+Doorgaan?";
+
+
+// Exporter ////////////////////////////////////////////////////////////////////
+"Nothing to export." = "Er is niets om te exporteren.";
+"No objects selected. Export entire model?" = "Geen objecten geselecteerd. Het gehele model exporteren?";
+"%s file location" = "%s bestandslocatie";
+
+"%i facets exported
+%i objects ignored"
+=
+"%i vlakken geëxporteerd
+%i objecten overgeslagen";
+
+// Export Options
+"STL Export Options" = "STL Export Opties";
+"Export unit: " = "Export eenheid: ";
+"File Format " = "Bestandsformaat ";
+"ASCII" = "ASCII";
+"Binary" = "Binair";
+
+// Menu Item
+"Export STL..." = "Export STL...";

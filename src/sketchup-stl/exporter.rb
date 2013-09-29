d94b1e23e369612f685747fc6af901d09682363f
Jim Foltz <jim.foltz@gmail.com>
2013-09-29 10:13:31 -0400

    Refactor to add SKUI dialog
    Adding SKUI involved some fairly major refactoring.
    
    * Added constant for OPTIONS.
    * Added method to get file extension (stl).
    * Added method to return model name.
    * Added method to select file path for export.
    * Added method to export model (decoupled from UI)
    * Added method to get scale factor from units.
    * Removed references to 'other' entities.
    * Added args to several methods to make them callable via tests.
    * Removed inputbox options dailog.
    * Added SKUI options dialog. ( do_options() )
    * Added export selected only option.
    * Updated .strings file to match dialog labels.
    * Other small tweaks, fixes, and clean-ups.

--- src/sketchup-stl/exporter.rb
+++ src/sketchup-stl/exporter.rb
@@ -10,139 +10,132 @@ module CommunityExtensions
   module STL
     module Exporter
 
-      STL_ASCII  = 'ascii'.freeze
-      STL_BINARY = 'binary'.freeze
+      STL_ASCII  = 'ASCII'.freeze
+      STL_BINARY = 'Binary'.freeze
+
+      OPTIONS = {
+        'selection_only' => false,
+        'export_units'   => 'Model Units',
+        'stl_format'     => STL_ASCII
+      }
 
       PREF_KEY = 'CommunityExtensions\STL\Exporter'.freeze
 
-      def self.export_mesh_file
-        model = Sketchup.active_model
-        model_name = File.basename(model.path, '.skp')
-        if model_name == ''
-          model_name = 'untitled'
-        end
-        @stl_conv = 1.0
-        @face_count = 0
-        @line_count = 0
-        # Whether the Selection or the Model is exported will depend on a new
-        # option in the export dialog.
-        # For now, just export the entre model.
-        export_ents = Sketchup.active_model.entities
-        # Get DXF export option.
-        file_type='stl'
-        # Get Export options.
-        options = options_dialog
-        return if options == false
-
-        @stl_conv = options[0]
-        @stl_type = options[1].downcase
-
-        # Get exported file name and export.
-        description = STL.translate('%s file location')
-        out_name = UI.savepanel(sprintf(description, file_type.upcase), nil,
-                                "#{model_name}.#{file_type}")
-        if out_name
-          @mesh_file = File.new(out_name , 'w')  
-          if @stl_type == STL_BINARY
-            @mesh_file.binmode
-            @write_face = method(:write_face_binary)
-          else
-            @write_face = method(:write_face_ascii)
-          end
-          write_header(model_name)
-
-          # Recursively export faces and edges, exploding groups as we go.
-          # Count "other" objects we can't parse.
-          others = find_faces(0, export_ents, Geom::Transformation.new)
-          write_footer(model_name)
-          message = STL.translate("%i facets exported\n%i objects ignored")
-          UI.messagebox(sprintf(message, @face_count, others))
-        end
+      def self.file_extension
+        'stl'
       end
 
-      def self.find_faces(others, entities, tform)
+      def self.model_name
+        title = Sketchup.active_model.title
+        title = "Untitled-#{Time.now.to_i.to_s(16)}" if title.empty?
+        title
+      end
+
+      def self.select_export_file
+        template  = STL.translate('%s file location')
+        file_name = "#{model_name()}.#{file_extension()}"
+        dlg_title = sprintf(template, file_name)
+        directory = nil
+        path = UI.savepanel(dlg_title, directory, file_name)
+      end
+
+      def self.export(path, options = OPTIONS)
+        file = File.new(path , 'w')  
+        if options['stl_format'] == STL_BINARY
+          file.binmode
+          @write_face = method(:write_face_binary)
+        else
+          @write_face = method(:write_face_ascii)
+        end
+        scale = scale_factor(options['export_units'])
+        write_header(file, model_name(), options['stl_format'])
+        # Recursively export faces and edges, exploding groups as we go.
+        if options['selection_only']
+          export_ents = Sketchup.active_model.selection
+        else
+          export_ents = Sketchup.active_model.active_entities
+        end
+        face_count = find_faces(file, export_ents, scale, Geom::Transformation.new)
+        write_footer(file, face_count, model_name(), options['stl_format'])
+      end
+
+      def self.find_faces(file, entities, scale, tform)
+        face_count = 0
         entities.each do |entity|
           if entity.is_a?(Sketchup::Face)
-            write_face(entity, tform)     
-          elsif entity.is_a?(Sketchup::Group) ||
-            entity.is_a?(Sketchup::ComponentInstance)
-            if entity.is_a?(Sketchup::Group)
-              # (!) Beware - Due to a SketchUp bug this can be incorrect.
-              # Fix later.
-              definition = entity.entities.parent
-            else
-              definition = entity.definition
-            end
-            others = find_faces(others, definition.entities,
-                                tform * entity.transformation)
-          else
-            others = others + 1
-          end
-        end # each
-        others
-      end
-
-      def self.write_face_ascii(mesh)
-        polygons = mesh.polygons
-        polygons.each do |polygon|
-          if (polygon.length == 3)
-            norm = mesh.normal_at(polygon[0].abs)
-            @mesh_file.write("facet normal #{norm.x} #{norm.y} #{norm.z}\n")
-            @mesh_file.write("  outer loop\n")
-            for j in 0..2 do
-              pt = mesh.point_at(polygon[j].abs)
-              pt = pt.to_a.map{|e| e * @stl_conv}
-              @mesh_file.write("    vertex #{pt.x} #{pt.y} #{pt.z}\n")
-            end
-            @mesh_file.write( "  endloop\nendfacet\n")
+            write_face(file, entity, scale, tform)     
+            face_count += 1
+          elsif entity.is_a?(Sketchup::Group)
+            # (!) Beware - Due to a SketchUp bug this can be incorrect.
+            # Fix later.
+            find_faces(file, entity.entities.parent, scale, tform * entity.transformation)
+          elsif entity.is_a?(Sketchup::ComponentInstance)
+            find_faces(file, entity.definition, scale, tform * entity.transformation)
           end
         end
+        face_count
       end
 
-      def self.write_face_binary(mesh)
-        polygons = mesh.polygons
-        polygons.each do |polygon|
-          if (polygon.length == 3)
-            norm = mesh.normal_at(polygon[0].abs)
-            @mesh_file.write(norm.to_a.pack("e3"))
-            for j in 0..2 do
-              pt = mesh.point_at(polygon[j].abs)
-              pt = pt.to_a.map{|e| e * @stl_conv}
-              @mesh_file.write(pt.pack("e3"))
-            end
-            @mesh_file.write([0].pack("v"))
-          end
-        end
-      end
-
-      def self.write_face(face, tform)
+      def self.write_face(file, face, scale, tform)
         mesh = face.mesh(7)
         mesh.transform!(tform)
-        @write_face.call(mesh)
-        @face_count += 1
+        @write_face.call(file, scale, mesh)
       end
 
-      def self.write_header(model_name)
-        if @stl_type == STL_ASCII
-          @mesh_file.write("solid #{model_name}\n")
+      def self.write_face_ascii(file, scale, mesh)
+        polygons = mesh.polygons
+        polygons.each do |polygon|
+          if (polygon.length == 3)
+            norm = mesh.normal_at(polygon[0].abs)
+            file.write("facet normal #{norm.x} #{norm.y} #{norm.z}\n")
+            file.write("  outer loop\n")
+            for j in 0..2 do
+              pt = mesh.point_at(polygon[j].abs)
+              pt = pt.to_a.map{|e| e * scale}
+              file.write("    vertex #{pt.x} #{pt.y} #{pt.z}\n")
+            end
+            file.write( "  endloop\nendfacet\n")
+          end
+        end
+      end
+
+      def self.write_face_binary(file, scale, mesh)
+        polygons = mesh.polygons
+        polygons.each do |polygon|
+          if (polygon.length == 3)
+            norm = mesh.normal_at(polygon[0].abs)
+            file.write(norm.to_a.pack("e3"))
+            for j in 0..2 do
+              pt = mesh.point_at(polygon[j].abs)
+              pt = pt.to_a.map{|e| e * scale}
+              file.write(pt.pack("e3"))
+            end
+            file.write([0].pack("v"))
+          end
+        end
+      end
+
+      def self.write_header(file, model_name, format)
+        if format == STL_ASCII
+          file.write("solid #{model_name}\n")
         else
-          @mesh_file.write(["SketchUp STL #{model_name}"].pack("A80"))
+          file.write(["SketchUp STL #{model_name}"].pack("A80"))
           # 0xffffffff is a place-holder value. In the binary format,
           # this value is updated in the write_footer method.
-          @mesh_file.write([0xffffffff].pack('V'))
+          file.write([0xffffffff].pack('V'))
         end
       end
 
-      def self.write_footer(model_name)
-        if @stl_type == STL_ASCII
-          @mesh_file.write("endsolid #{model_name}\n")
+      def self.write_footer(file, face_count, model_name, format)
+        if format == STL_ASCII
+          file.write("endsolid #{model_name}\n")
         else
           # binary - update facet count
-          @mesh_file.flush
-          @mesh_file.seek(80)
-          @mesh_file.write([@face_count].pack('V'))
+          file.flush
+          file.seek(80)
+          file.write([face_count].pack('V'))
         end
-        @mesh_file.close
+        file.close
       end
 
       # Wrapper to shorten the syntax and create a central place to modify in case
@@ -172,56 +165,133 @@ module CommunityExtensions
         end
       end
 
-      def self.options_dialog
-        formats = %w(ASCII Binary)
-        units   = ['Model Units', 'Meters', 'Centimeters', 'Millimeters', 'Inches', 'Feet']
-        formats_translated = formats.map { |format| STL.translate(format) }
-        units_translated   = units.map   { |unit| STL.translate(unit) }
-        prompts  = [
-          STL.translate('Export unit: '),
-          STL.translate('File Format ')
-        ]
-        choices  = [
-          units_translated.join('|'),
-          formats_translated.join('|')
-        ]
-        defaults = [
-          STL.translate(read_setting('export_units', 'Model Units')),
-          STL.translate(read_setting('stl_format', 'ASCII'))
-        ]
-        title = STL.translate('STL Export Options')
-        results = UI.inputbox(prompts, defaults, choices, title)
-        return false if results == false
-        if results[0] == STL.translate('Model Units')
-          selected_units = model_units()
+      def self.scale_factor(unit_key)
+        if unit_key == 'Model Units'
+          selected_key = model_units()
         else
-          selected_units = results[0]
+          selected_key = unit_key
         end
-        case selected_units
-        when STL.translate('Meters')
-          stl_conv = 0.0254
-        when STL.translate('Centimeters')
-          stl_conv = 2.54
-        when STL.translate('Millimeters')
-          stl_conv = 25.4
-        when STL.translate('Feet')
-          stl_conv = 0.0833333333333333
-        when STL.translate('Inches')
-          stl_conv = 1
+        case selected_key
+        when 'Meters'
+          factor = 0.0254
+        when 'Centimeters'
+          factor = 2.54
+        when 'Millimeters'
+          factor = 25.4
+        when 'Feet'
+          factor = 0.0833333333333333
+        when 'Inches'
+          factor = 1.0
         end
-        #  Important to get the English value back from the format as the
-        #  English string is expected and used for later processing of
-        #  the options.
+        factor
+      end
+
+      def self.do_options
+        # Load SKUI lib
+        unless defined?(SKUI)
+          load File.join(File.dirname(__FILE__), 'SKUI', 'core.rb')
+        end
+
+        # Read last saved options
+        ['selection_only', 'stl_format', 'export_units'].each do |key|
+          OPTIONS[key] = read_setting(key, OPTIONS[key])
+        end
+
+        units = ['Model Units', 'Meters', 'Centimeters', 'Millimeters', 'Inches', 'Feet']
+        units_translated = units.map { |unit| STL.translate(unit) }
+
+        formats = %w(ASCII Binary)
+        formats_translated = formats.map { |format| STL.translate(format) }
+
+        # Columns and Rows for control alignment
+        c = [0, 10, 110]
+        first_row = 7
+        vspace = 30
+        r = (0..4).map{|e| e * vspace + first_row}
+        r.unshift(0)
+
+        window_options = {
+          :title           => STL.translate('STL Export Options'),
+          :preferences_key => PREF_KEY,
+          :height => 160,
+          :width  => 290
+        }
+        w = SKUI::Window.new(window_options)
+
+        # Row 1 Export Selected
+        chk_selection = SKUI::Checkbox.new(
+          'Export selected geometry only.',
+          OPTIONS['selection_only']
+        )
+        chk_selection.position(c[1], r[1])
+        chk_selection.check! if OPTIONS['selection_only']
+        chk_selection.on(:change) { |control|
+          OPTIONS['selection_only'] = control.checked?
+        }
+        w.add_control(chk_selection)
+
         #
-        #  Also using the English unit name in the Registry.
-        unit_index   = units_translated.index(results[0])
-        write_setting('export_units', units[unit_index])
+        # Row 2 Export Units
+        #
+        lst_units = SKUI::Listbox.new(units_translated)
+        lst_units.position(c[2], r[2])
+        lst_units.width = 169
+        lst_units.value = STL.translate(OPTIONS['export_units'])
+        lst_units.on( :change ) { |control, value|
+          unit_index = units_translated.index(value)
+          OPTIONS['export_units'] = units[unit_index]
+        }
+        w.add_control(lst_units)
 
-        format_index = formats_translated.index(results[1])
-        write_setting('stl_format', formats[format_index])
+        lbl_units = SKUI::Label.new(STL.translate('Export unit:'), lst_units)
+        lbl_units.position(c[1], r[2])
+        w.add_control(lbl_units)
 
-        return [stl_conv, formats[format_index]]
-      end # options_dialog()
+        #
+        # Row 3 File Format field
+        #
+        lst_format = SKUI::Listbox.new(formats_translated)
+        lst_format.value = lst_format.items.first
+        lst_format.position(c[2], r[3])
+        lst_format.width = 169
+        lst_format.value = STL.translate(OPTIONS['stl_format'])
+        lst_format.on( :change ) { |control, value|
+          format_index = formats_translated.index(value)
+          OPTIONS['stl_format'] = formats[format_index]
+        }
+        w.add_control( lst_format )
+
+        lbl_type = SKUI::Label.new(STL.translate('File format:'), lst_format)
+        lbl_type.position(c[1], r[3])
+        w.add_control(lbl_type)
+
+        #
+        # Export and Cancel Buttons
+        #
+        btn_export = SKUI::Button.new('Export') { |control|
+
+          write_setting('export_units'   , OPTIONS['export_units'])
+          write_setting('stl_format'     , OPTIONS['stl_format'])
+          write_setting('selection_only' , OPTIONS['selection_only'])
+
+          path = select_export_file()
+
+          export(path, OPTIONS) unless path.nil?
+          control.window.close
+        }
+
+        btn_export.position(125, -5 )
+        w.add_control(btn_export)
+
+        btn_cancel = SKUI::Button.new('Cancel') { |control|
+          control.window.close
+        }
+        btn_cancel.position(-5, -5 )
+        w.add_control(btn_cancel)
+
+        w.default_button = btn_export
+        w.show
+      end # do_options
 
       unless file_loaded?(__FILE__)
         # Pick menu indexes for where to insert the Export menu. These numbers
@@ -234,11 +304,11 @@ module CommunityExtensions
         # (i) The menu_index argument isn't supported by older versions.
         if Sketchup::Menu.instance_method(:add_item).arity == 1
           item = UI.menu('File').add_item(STL.translate('Export STL...')) {
-            export_mesh_file
+            do_options
           }
         else
           item = UI.menu('File').add_item(STL.translate('Export STL...'), insert_index) {
-            export_mesh_file
+            do_options
           }
         end
         UI.menu('File').set_validation_proc(item) do
